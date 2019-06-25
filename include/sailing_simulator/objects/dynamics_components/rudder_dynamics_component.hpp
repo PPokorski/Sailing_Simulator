a951b7fc0764b41422c5f9b387338db5ca3fc556
PPokorski <piotr.pokorski94@gmail.com>
2019-06-26 01:12:42 +0200

    Updated copyright

--- include/sailing_simulator/objects/dynamics_components/rudder_dynamics_component.hpp
+++ include/sailing_simulator/objects/dynamics_components/rudder_dynamics_component.hpp
@@ -2,7 +2,7 @@
 *
 * BSD 3-Clause License
 *
-*  Copyright (c) 2019, Piotr Pokorski, Piotr Rzewnicki
+*  Copyright (c) 2019, Piotr Pokorski
 *  All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without
